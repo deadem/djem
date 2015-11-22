b045968923c78479e0fe48f1d3ae719024778246
deadem <deadem@gmail.com>
2015-11-22 19:28:01 +0300

    DJEM-31 #time 2h исправлены вызовы в типах документов

--- app/Doctype.php
+++ app/Doctype.php
@@ -1,23 +1,29 @@
 <?php
 namespace DJEM;
 
-/*
-example route:
-    Route::get('{any}', function($url) { return App\Doctypes\Doctype::view($url, App\Models\Url::class); })->where('any', '(?!djem/).*');
+// @codingStandardsIgnoreStart
+//
+// example route:
+// Route::get('{any}', function ($url) {
+//    return (new App\Doctypes\Doctype)->view('/'.$url, App\Models\Url::class);
+// })->where('any', '(?!djem/).*');
+// 
+// example URL Model
+//     namespace App\Models;
+//     ...
+//     class Url extends ...
+//     {
+//         protected $table = 'url';
+//         protected $primaryKey = 'url';
+//         protected $hidden = [ 'url', 'doctype', 'model', 'refid' ];
+//         ...
+//     }
+//
+// @codingStandardsIgnoreEnd
 
-example URLModel
-    namespace App\Models;
-    ...
-    class Url extends ...
-    {
-        protected $table = 'url';
-        protected $primaryKey = 'url';
-        protected $hidden = [ 'url', 'doctype', 'model', 'refid' ];
-        ...
-    }
-
-*/
-
+/**
+ * базовый тип документа.
+ */
 class Doctype extends \Illuminate\Routing\Controller
 {
     /**
@@ -27,37 +33,37 @@ class Doctype extends \Illuminate\Routing\Controller
 
     /**
      * Поиск данных для отображения по URL
-     * @param string $url URL
-     * @param string $urlModel модель, которая обрабатывает URL и хранит данные о них
-     * @return object класс с полями doctype, model, refid для отображения соответствующим 
-     *         типом модели, найденной по идентификатору
+     * @param string $url      URL.
+     * @param string $urlModel модель, которая обрабатывает URL и хранит данные о них.
+     * @return object класс с полями doctype, model, refid для отображения соответствующим
+     *         типом модели, найденной по идентификатору.
      */
-    public static function view($url, $urlModel)
+    public function view($url, $urlModel)
     {
-        return self::viewAddress($urlModel::where('url', '=', $url)->first());
+        return $this->viewAddress($urlModel::where('url', '=', $url)->first());
     }
 
     /**
      * Загрузка из объекта URL данных из типа документа и вызов метода renderView для отображения
-     * @param object $address с полями doctype, model, refid
+     * @param object $address с полями doctype, model, refid.
      * @return object данные для отображения
      */
-    public static function viewAddress($address)
+    public function viewAddress($address)
     {
         if (!$address) {
             abort(404, 'File not found');
         }
-        $doctype = self::getDocType($address->doctype);
-        return $doctype::renderView($address->model, $address->refid);
+        $doctype = $this->getDocType($address->doctype);
+        return (new $doctype)->renderView($address->model, $address->refid);
     }
 
     /**
      * Перегружаемая функция, рендерит указанную модель
-     * @param string $model модель
-     * @param string $id идентификатор
-     * @return object данные для отображения 
+     * @param string $model модель.
+     * @param string $id    идентификатор.
+     * @return object данные для отображения.
      */
-    static public function renderView($model, $id)
+    public function renderView($model, $id)
     {
         return [ 'model' => $model, 'id' => $id ];
     }
@@ -140,6 +146,7 @@ class Doctype extends \Illuminate\Routing\Controller
         $fields = [];
         $columns = [];
         $options = [];
+
         foreach ($this->fields($id) as $row) {
             $field = [];
             $column = [];
@@ -155,16 +162,20 @@ class Doctype extends \Illuminate\Routing\Controller
                             $column['dataIndex'] = $value;
                         }
                         break;
+
                     case 'type':
                         $field[$key] = $value;
                         break;
+
                     case 'sortable':
                     case 'text':
                     case 'flex':
                     case 'width':
                         $column[$key] = $value;
                         break;
+
                     default:
+                        // неизвестные параметры не обрабатываем
                         break;
                 }
             }
