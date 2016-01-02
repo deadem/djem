d4297a1c8f800eb792a0bc135e476f2de2a5a978
deadem <deadem@gmail.com>
2016-01-02 15:31:16 +0300

    добавлена заготовка для дерева

--- config/djem.php
+++ config/djem.php
@@ -2,9 +2,12 @@
 
 return [
     'main' => '\DJEM\Http\Controllers\Api\Main',
+    'tree' => function () {
+        return [
+            'items' => [
+                [ 'id' => 1, 'text' => 'Главные страницы', 'leaf' => true, '_doctype' => App\Doctypes\Doctype::class ],
+            ]
+        ];
+    },
     'resources' => __DIR__.'/../resources/djem',
-    'doctypes' => [
-        App\Doctypes\Doctype::class
-    ],
-    'models' => []
 ];
