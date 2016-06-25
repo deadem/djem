60dab3b793c1817e526f34f29a2cbeed01f35add
deadem <deadem@gmail.com>
2016-06-25 10:57:17 +0300

    изменён наследник, оставлены только нужные переопределения, добавлено базовое отображение закладок верхнего тулбара

--- panel/app/view/main/Main.js
+++ panel/app/view/main/Main.js
@@ -28,7 +28,6 @@ Ext.define('djem.view.main.Main', {
     items: [ {
         region: 'north',
         xtype: 'main-toolbar',
-        cls: 'top-toolbar',
         reference: 'toolbar'
     }, {
         region: 'center',
