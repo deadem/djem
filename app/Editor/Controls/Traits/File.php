5fd637a557d69bfcb9067cd6ba2b5e5bcdb39966
deadem <deadem@gmail.com>
2016-10-10 00:25:30 +0300

    исправлено получение файлов

--- app/Editor/Controls/Traits/File.php
+++ app/Editor/Controls/Traits/File.php
@@ -6,13 +6,25 @@ use InvalidArgumentException;
 
 trait File
 {
-    private function getFilePath($file)
+    private function getFilePath($fileName)
     {
-        $file = realpath(sys_get_temp_dir().'/'.$file);
+        $url = parse_url($fileName);
+        if (isset($url['scheme']) && isset($url['path'])) {
+            $newFileName = uniqid().'.'.basename($url['path']);
+            $file = @file_get_contents($fileName, false, stream_context_create(['ssl' => ['verify_peer' => false]]));
+            if (empty($file)) {
+                return false;
+            }
+            if (! file_put_contents(sys_get_temp_dir().'/'.$newFileName, $file)) {
+                return false;
+            }
+            $fileName = $newFileName;
+        }
 
+        $file = realpath(sys_get_temp_dir().'/'.$fileName);
         if (substr($file, 0, strlen(sys_get_temp_dir())) !== sys_get_temp_dir()) {
             // какой-то неправильный файл, игнорируем
-            throw new InvalidArgumentException('Invalid filename ['.$file.'].');
+            throw new InvalidArgumentException('Invalid filename ['.$fileName.'].');
         }
 
         return $file;
