3d3f4c9312d576cc49fbe60f558257020f25f909
MOE <duba@list.ru>
2016-12-15 17:23:10 +0300

    delete alias

--- panel/app/widget/gridImage.js
+++ panel/app/widget/gridImage.js
@@ -1,7 +1,6 @@
 /* global Ext */
 Ext.define('djem.widget.gridImage', {
     extend: 'Ext.grid.column.Column',
-    alias: ['djem.widget.image'],
 
     renderer: function (value) {
         return Ext.String.format(['<div class="image" style="background-image: url({0})"></div>'], value);
