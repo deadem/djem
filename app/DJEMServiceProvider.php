8ecef251eb21dcc9c67f6b8bc06c01ff3f2e4543
Eugene Sinitsyn <phollyolly@gmail.com>
2017-02-07 17:07:38 +0300

    поправлена привязка middlware для новых версий laravel

--- app/DJEMServiceProvider.php
+++ app/DJEMServiceProvider.php
@@ -10,7 +10,11 @@ class DJEMServiceProvider extends ServiceProvider
 {
     private function routes(Router $router)
     {
-        $router->middleware('djem.auth', \DJEM\Http\Middleware\Authenticate::class);
+        if(method_exists($router, 'middleware')) {
+            $router->middleware('djem.auth', \DJEM\Http\Middleware\Authenticate::class);
+        } else {
+            $router->aliasMiddleware('djem.auth', \DJEM\Http\Middleware\Authenticate::class);
+        }
         $router->middlewareGroup('djem.web', [
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
