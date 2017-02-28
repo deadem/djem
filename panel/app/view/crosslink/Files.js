76b77db756c9d56585272b0633ba6030f6bafcd2
MOE <duba@list.ru>
2017-02-28 11:05:22 +0300

    drag`n`drop for sorting images

--- panel/app/view/crosslink/Files.js
+++ panel/app/view/crosslink/Files.js
@@ -3,16 +3,49 @@ Ext.define('djem.view.crosslink.Files', {
   extend: 'Ext.view.View',
   alias: 'widget.crosslink.Files',
 
+  config: { selectionModel: { mode: "MULTI" } },
+
   cls: 'x-form-crosslink-files-wrap',
   requires: [
     'djem.view.crosslink.FilesController', 'djem.view.crosslink.FilesModel', 'djem.view.crosslink.FileField',
     'djem.store.CrossLink', 'djem.model.Files', 'djem.view.crosslink.Editor'
   ],
 
+  deleteImages: function(record) {
+    var view = this;
+
+    if (view.getSelection().length) {
+      Ext.each(view.getSelection(), function(selection) { selection.drop(); });
+    } else {
+      view.getStore().remove(record);
+    }
+  },
+
+  moveItemOnFirst: function(record) {
+    var store = this.getStore();
+    store.remove(record);
+    store.insert(0, record);
+  },
+
   listeners: {
     afterrender: { fn: 'initAfterRender', options: { single: true } },
     beforedestroy: 'onBeforeDestroy',
-    itemdblclick: 'onItemDblClick'
+    itemdblclick: 'onItemDblClick',
+    itemcontextmenu: function(view, record, item, index, e) {
+      var me = this;
+      var contextMenu = new Ext.menu.Menu({
+        items: [
+          { text: "Удалить выделенные", glyph: "xF1C0@Icons", handler: function() { return me.deleteImages(record); } },
+          {
+            text: "Переместить в начало",
+            glyph: "xF05B@Icons",
+            handler: function() { return me.moveItemOnFirst(record); }
+          }
+        ]
+      });
+      contextMenu.showAt(e.getXY());
+      e.stopEvent();
+    }
   },
 
   controller: 'crosslink-files',
@@ -50,8 +83,9 @@ Ext.define('djem.view.crosslink.Files', {
   itemSelector: 'div.thumb-wrap',
   tpl: [
     '<tpl for=".">', '<div class="thumb-wrap {new}">', '<a href="#" class="trash">&#xF156;</a>',
-    '<div class="thumb" style="background-repeat: no-repeat;',
-    'background-image: url({url});background-position:{calcOffset};background-size:100%;background-size:{calcZoom}">',
-    '</div>', '<span>{name}</span>', '</div>', '</tpl>'
+    '<div class="thumb" style="background-repeat: no-repeat;background-image: url({url});"></div>',
+    '<span>{name}</span>', '</div>', '</tpl>', '<label>', '<div class="icon"></div>', '<input type="file" ',
+    'multiple="" ', ' onchange="Ext.get(this.parentNode.parentNode).fireEvent(\'filechange\', event, this);">',
+    '</label>'
   ]
 });
